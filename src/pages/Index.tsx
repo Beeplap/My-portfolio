7eb941df6b4f88ac0d10526abcec2c960f91318b
beeplap <ghartibeeplap@gmail.com>
2025-10-09 22:46:52 +0545

    Integrate Hero component into Index page for enhanced homepage layout

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,12 +1,12 @@
 import Navigation from "@/components/Navigation";
-
+import Hero from "@/components/Hero";
 
 const Index = () => {
   return (
     <div className="min-h-screen bg-background">
       <Navigation />
       <main>
-        
+        <Hero />
       </main>
      
     </div>
